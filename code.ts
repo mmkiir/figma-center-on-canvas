0e048f09a31e36c40e3b686f281cf508286b82d0
mmkiir <porasjeet@gmail.com>
2021-02-25 00:29:23 +0530

    feat: add functionality to center selection on canvas

--- code.ts
+++ code.ts
@@ -1,20 +1,20 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+function main() {
+  if (figma.currentPage.selection.length === 0) {
+    figma.closePlugin("No nodes selected");
+    return;
+  }
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+  const group = figma.group(figma.currentPage.selection, figma.currentPage);
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+  group.x = (-1 * group.width) / 2;
+  group.y = (-1 * group.height) / 2;
+
+  for (const node of group.children) {
+    figma.currentPage.appendChild(node);
+  }
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+
+main();
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.
